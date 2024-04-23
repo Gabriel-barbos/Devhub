f24c6069670650e1d73301047cefde1c13354e6b
gxbizz <gabriela.fioriizzo@gmail.com>
2024-04-22 22:40:13 -0300

    CRUD: images

--- server/router/routes.py
+++ server/router/routes.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, File, UploadFile, HTTPException
+from fastapi.responses import FileResponse
+import os
+
 from models.User import User
 from db_config import usersCollection
 router = APIRouter()
@@ -17,3 +20,35 @@ def get_all_users():
 def create_user(user: User):
     usersCollection.insert_one(dict(user))
     return {user.model_dump_json()}
+
+# rotas imagens:
+IMAGES_DIRECTORY = "imagens"
+
+@app.post("/upload/")
+async def upload_image(file: UploadFile = File(...)):
+    if not os.path.exists(IMAGES_DIRECTORY):
+        os.makedirs(IMAGES_DIRECTORY)
+
+        file_path = os.path.join(IMAGES_DIRECCTORY, file.filename)
+        with open(file_path, "wb") as image:
+            image.writer(await file.read())
+            
+        return {"filename": file.filename}
+    
+
+@app.get("/images/{filename}")
+async def get_image(filename: str):
+    file_path = os.path.join(IMAGES_DIRECTORY, filename)
+    if os.path.exists(file_path):
+        return FileResponse(file_path)
+    else:
+        raise HTTPException(status_code=404, detail="Imagem não encontrada")
+
+@app.delete("/images/{filename}")
+async def delete_image(filename: str):
+    file_path = os.path.join(IMAGES_DIRECTORY, filename)
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        return {"message": f"Imagem {filename} excluída com sucesso."}
+    else:
+        raise HTTPException(status_code=404, detail="Imagem não encontrada")   
