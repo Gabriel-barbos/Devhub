fcb806bde4cac7ec222dd9e189caf1110240480a
gabrielmeiradev <gabriel_meira10@hotmail.com>
2024-04-24 18:32:05 -0300

    style: changing profile tabs font size

--- client/components/profile-page/profile-tabs.tsx
+++ client/components/profile-page/profile-tabs.tsx
@@ -1,9 +1,9 @@
 const ProfileTabs = () => {
     return (
     <div className="flex">
-        <a href="#" className={"flex-1 font-semibold text-lg"}>Posts</a>
-        <a href="#" className={"flex-1 text-center text-lg text-neutral-400"}>Artigos</a>
-        <a href="#" className={"flex-1 text-right text-lg text-neutral-400"}>Projetos</a>
+        <a href="#" className={"flex-1 font-semibold text-base"}>Posts</a>
+        <a href="#" className={"flex-1 text-center text-base text-neutral-400"}>Artigos</a>
+        <a href="#" className={"flex-1 text-right text-base text-neutral-400"}>Projetos</a>
     </div> )
 }
 
