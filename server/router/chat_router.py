1bd4ee534a9787f8934252b77f78d0336804dd34
gxbizz <gabriela.fioriizzo@gmail.com>
2024-05-15 10:58:38 -0300

    feat: chat router

---
+++ server/router/chat_router.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, HTTPException,FastAPI, Depends, status,File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+
+import requests
+
+chat_router = APIRouter(tags=['Chat'])
+
+PRIVATE_KEY = "3ed0751a-b5ae-4b2c-8b6f-109afc5818b2"
+
+class User(BaseModel):
+    username: str
+    password: str
+    name: str
+
+@chat_router.post('/authenticate')
+async def authenticate(user: User):
+    response = requests.put('https://api.chatengine.io/users/',
+        data={
+            "username": user.username,
+            "secret": user.password,
+            "first_name": user.name,
+        },
+        headers={ "Project-ID": PRIVATE_KEY }
+    )
+    return response.json()
