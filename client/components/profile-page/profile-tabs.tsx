38790a0121bb465bae05c0660d85e5bb6283e783
Gabriel-barbos <126466365+Gabriel-barbos@users.noreply.github.com>
2024-05-21 22:19:49 -0300

    feat: Articles

--- client/components/profile-page/profile-tabs.tsx
+++ client/components/profile-page/profile-tabs.tsx
@@ -1,8 +1,10 @@
-const ProfileTabs = () => {
+
+
+const ProfileTabs = (username) => {
     return (
     <div className="flex sticky top-0 py-4 bg-gradient-to-b from-background to-transparent">
         <a href="#" className={"flex-1 text-center font-semibold text-base"}>Posts</a>
-        <a href="#" className={"flex-1 text-center text-base text-neutral-400"}>Artigos</a>
+        <a href= {`/${username}/articles`} className={"flex-1 text-center text-base text-neutral-400"}>Artigos</a>
         <a href="#" className={"flex-1 text-center text-base text-neutral-400"}>Projetos</a>
     </div> )
 }
