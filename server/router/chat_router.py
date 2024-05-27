7ded2cf91ced5132590c8ef27f141ecc909ad3fc
gabrielmeiradev <gabriel_meira10@hotmail.com>
2024-05-27 20:15:49 -0300

    feat: introducing chat

--- server/router/chat_router.py
+++ server/router/chat_router.py
@@ -1,26 +1,35 @@
-from fastapi import APIRouter,requests, HTTPException,FastAPI, Depends, status,File, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
-
-from pydantic import BaseModel
-
+from fastapi import APIRouter, Form, HTTPException, WebSocket
+from datetime import datetime
+from models.Message import Message
+from db_config import messagesCollection as collection
 
 chat_router = APIRouter(tags=['Chat'])
 
-PRIVATE_KEY = "3ed0751a-b5ae-4b2c-8b6f-109afc5818b2"
+@chat_router.post("/chat/{username}/messages")
+def create_message(username: str, from_user: str, content: str):
+    try:
+        message_dict = {
+            "from_user": from_user,
+            "to_user": username,
+            "content": content,
+            "datetime": datetime.utcnow()
+        }
+        result = collection.insert_one(message_dict)
+        if result.inserted_id:
+            created_message = collection.find_one({"_id": result.inserted_id})
+            return {"message": "Mensagem criada"}
+    except Exception as error:
+            raise HTTPException(status_code=500, detail="A mensagem nao pode ser criada")
+    
 
-class User(BaseModel):
-    username: str
-    password: str
-    name: str
-
-@chat_router.post('/authenticate')
-async def authenticate(user: User):
-    response = requests.put('https://api.chatengine.io/users/',
-        data={
-            "username": user.username,
-            "secret": user.password,
-            "first_name": user.name,
-        },
-        headers={ "Project-ID": PRIVATE_KEY }
-    )
-    return response.json()
+@chat_router.get("/chat/{from_username}/{to_username}/messages")
+def get_messages(from_username: str, to_username: str):
+    try:
+        messages = collection.find({"$and": [{"from_user": from_username}, {"to_user": to_username}]}, {'_id': 0})
+        result = []
+        for message in messages:
+            result.append(message)
+        return result
+    except Exception as error:
+         raise HTTPException(status_code=500, detail="Ocorreu um erro ao buscar mensagens")
+    
