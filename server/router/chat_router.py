fc4c3d822bd357b90d19b5a9e9fc30e53f7dbd0a
Gabriel Meira <gabriel_meira10@hotmail.com>
2024-05-27 17:52:17 -0300

    fix: chat feature

--- server/router/chat_router.py
+++ server/router/chat_router.py
@@ -4,6 +4,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 @chat_router.post('/chat/{username}/messages')
-async def create_message(username: str, from_username: str, to_username: str, ):
+async def create_message(username: str, from_username: str, to_username: str, content: str):
     # aqui a lógica de registrar mensagens no banco de dados
     # depois fazer a lógica dos sockets
