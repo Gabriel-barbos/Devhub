7648dddbb8ad5001df068652f5ddb93758a3901c
gabrielmeiradev <gabriel_meira10@hotmail.com>
2024-06-05 14:10:13 -0300

    wip: follow button

--- client/components/shared/posts-list.tsx
+++ client/components/shared/posts-list.tsx
@@ -6,9 +6,8 @@ const PostsList = ({ posts, name, auth, imageUrl }) => {
     return (
         <div className="py-4 flex flex-col gap-4">
             {posts.toReversed().map((post, i) => {
-                return <Post key={i} reply_to={post["reply_to"]} user={{name: name, username: post["author_username"]}} likes_count={post["countLikes"] || 0} content={post["content"]} created_at={post["created_at"]} id={post["id"]} auth={auth} imageUrl={imageUrl}/>
+                return <Post key={i} reply_to={post["reply_to"]} user={{name: name, username: post["author_username"]}} likes_count={post["likes_count"] || 0} content={post["content"]} created_at={post["created_at"]} id={post["id"]} auth={auth} imageUrl={imageUrl}/>
             })}
-           
         </div>
     )
 }
